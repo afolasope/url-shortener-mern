3ec646b6939e6cfe70f30f62087b8769c0cb8a86
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2023-06-23 00:33:07 +0100

    update config

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,13 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 const development = process.env.NODE_ENV == 'development';
 
-
 export const config = {
-    HOST : development ? 'localhost:8000' : undefined
-}
+  HOST: development ? 'localhost:8000' : window.location.host,
+  BASE_URL: development ? 'http://localhost:8000' : undefined,
+};
 
 export const axiosInstance = axios.create({
-    baseURL: config.HOST,
+  baseURL: config.BASE_URL,
 });
-
